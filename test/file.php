e5f3e2f981024863278b960ddc11756ac57af3b8
lmsoto <luis.miguel.soto@monsanto.com>
2018-07-23 16:07:24 -0300

    Working with services and interfaces

---
+++ test/file.php
@@ -0,0 +1,34 @@
+<?php 
+	header ('Access-Control-Allow-Origin: *');
+	header ('Content-Type: application/json');
+
+?>
+
+{
+	"obj": [
+		{
+			"name" : "Luis",
+			"online" : true
+		},
+		{
+			"name" : "Lucho",
+			"online" : false
+		},
+		{
+			"name" : "Liclok",
+			"online" : true
+		},
+		{
+			"name": "Juancho",
+			"online" : true
+		},
+		{
+			"name" : "Pepe",
+			"online" : false
+		},
+		{
+			"name" : "Checo",
+			"online" : true
+		}
+	]
+}
